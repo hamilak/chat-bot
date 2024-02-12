6fc923b1f81bcdb0d45783b06cdb5ef7bea41f87
kalimah <arigbabu.kalimah@gmail.com>
2024-02-12 23:22:01 +0100

    feat: initialize nest and integrate openai

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { config as dotenvConfig } from 'dotenv';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+dotenvConfig({ path: '.env' });
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'frontend', 'dist'),
+    }),
+    ChatModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ apps/backend/src/chat/chat.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ChatService } from './chat.service';
+
+@Controller('chat')
+export class ChatController {
+  constructor(private chatService: ChatService) {}
+
+  @Post()
+  async chatCompletion(@Body('chats') chats) {
+    const result = await this.chatService.chatCompletion(chats);
+    console.log(result);
+    return result;
+  }
+}
